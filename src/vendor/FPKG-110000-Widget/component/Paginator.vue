59dd06a214067016eeeb13b7bfec434e023b2a43
Ruby Chen <mp4bj6ruby@gmail.com>
2018-07-27 16:23:28 +0800

    建立Paginator組件

---
+++ src/vendor/FPKG-110000-Widget/component/Paginator.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="Paginator">
+    <el-pagination
+      v-if="count > 0"
+      layout="prev, pager, next"
+      @current-change="onPageChanged"
+      :page-size="perpage"
+      :total="count">
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    perpage: {
+      default: 50,
+      type: Number
+    },
+    count: {
+      default: 0,
+      type: Number
+    },
+    onPageChanged: {
+      default: (page) => {
+        console.log("current page: " + page)
+      },
+      type: Function
+    }
+  }
+}
+
+</script>
+
+<style lang="stylus">
+.Paginator
+  text-align: right
+  padding: 10px 0
+</style>
