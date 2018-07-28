ac6fa725735014b33982a7960a0e8cf5b66b4535
Ruby Chen <mp4bj6ruby@gmail.com>
2018-07-28 10:12:17 +0800

    time tool modify

--- src/vendor/FPKG-120000-Util/time.js
+++ src/vendor/FPKG-120000-Util/time.js
@@ -1,5 +1,5 @@
 import moment from 'moment'
-export const getDateTime = (dt) => moment(dt).format('YYYY-MM-DD HH:mm:ss')
+export const getDateTime = (dt) => dt ? moment(dt).format('YYYY-MM-DD HH:mm:ss') : "-"
 export const startAtDay = (t) => moment(t).startOf('day').format('YYYY-MM-DD HH:mm:ss')
 export const endAtDay = (t) => moment(t).endOf('day').format('YYYY-MM-DD HH:mm:ss')
 export const dateAfter = (ta, tb) => moment(ta) < moment(tb)
