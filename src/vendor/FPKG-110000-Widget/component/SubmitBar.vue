052ac93c2b302852307db6e2f1de9bac2667d95d
Ruby Chen <mp4bj6ruby@gmail.com>
2018-08-07 17:39:49 +0800

    新增小工具:SubmitBar

---
+++ src/vendor/FPKG-110000-Widget/component/SubmitBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="SubmitBar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+  }
+}
+
+</script>
+
+<style lang="stylus">
+#SubmitBar
+  background-color: #eee 
+  border-radius: 5px 
+  padding: 8px 10px
+  margin-top: 20px
+  text-align: right
+</style>
