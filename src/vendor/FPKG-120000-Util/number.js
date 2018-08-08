c3b7a7c62d6045d43a3cb94d13ca66098f589520
Ruby Chen <mp4bj6ruby@gmail.com>
2018-08-08 15:33:46 +0800

    toCurrencyDecimal改為可自訂小數位數

--- src/vendor/FPKG-120000-Util/number.js
+++ src/vendor/FPKG-120000-Util/number.js
@@ -1,5 +1,6 @@
 import numeral from 'numeral'
 import Vue from 'vue'
+import _ from 'lodash'
 
 export const toCurrency = (num) => numeral(num).format('0,0');
-export const toCurrencyDecimal = (num) => numeral(num).format('0,0.00');
+export const toCurrencyDecimal = (num, decimalCount = 2) => numeral(num).format('0,0.' + _.repeat('0', decimalCount));
