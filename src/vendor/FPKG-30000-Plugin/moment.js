51002da4b9caa79009872d4ee30a83854d6db427
Ruby Chen <mp4bj6ruby@gmail.com>
2018-08-10 14:32:47 +0800

    Plugin:Moment: 新增Config設定: 一週開始日改為週一(原為週日)

--- src/vendor/FPKG-30000-Plugin/moment.js
+++ src/vendor/FPKG-30000-Plugin/moment.js
@@ -1,3 +1,10 @@
 import Vue from 'vue';
 import Moment from 'moment';
+Moment.updateLocale('zh-tw', {
+  week: {
+    dow: 1,
+  },
+})
 Object.defineProperty(Vue.prototype, '$moment', { value: Moment });
+
+export default Moment
