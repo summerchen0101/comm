99572607daf1f8053e5a6616b4f79cfe7cb09465
Ruby Chen <mp4bj6ruby@gmail.com>
2018-08-10 14:28:38 +0800

    config:枚舉值增加會員資訊相關

--- src/vendor/FPKG-10000-Config/enum.js
+++ src/vendor/FPKG-10000-Config/enum.js
@@ -13,13 +13,33 @@ export const loginStatus = [
   {label: '正常登出', value: 2},
   {label: '強制登出', value: 3},
 ]
-export const dispenseTicketStatus = [
+export const ticketStatus = [
   {label: '暫存', value: 0},
   {label: '成功', value: 1},
   {label: '審核處理', value: 2},
   {label: '取消', value: 3},
   {label: '出款中', value: 4},
   {label: '出款取消', value: 5},
+]
+export const bankAccStatus = [
+  {label: '成功', value: 1},
+  {label: '審核處理', value: 2},
+]
+export const memberLevelActive = [
+  {label: '啟用', value: 1},
+  {label: '停用', value: 2},
+]
+export const memberLevel = [
+  {label: '1', value: 1},
+  {label: '2', value: 2},
+  {label: '3', value: 3},
+  {label: '4', value: 4},
+  {label: '5', value: 5},
+  {label: '6', value: 6},
+  {label: '7', value: 7},
+  {label: '8', value: 8},
+  {label: '9', value: 9},
+  {label: '10', value: 10},
 ]
 export const gameType = [
   {
