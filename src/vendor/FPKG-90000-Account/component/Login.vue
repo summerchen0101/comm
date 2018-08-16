462779ae49ab78d867c0a55055f5dcd6c32d0b56
Ruby Chen <mp4bj6ruby@gmail.com>
2018-08-16 18:02:57 +0800

    登入頁的LoadingCover微調

--- src/vendor/FPKG-90000-Account/component/Login.vue
+++ src/vendor/FPKG-90000-Account/component/Login.vue
@@ -42,7 +42,7 @@
         </b-form>
       </div>
     </div>
-    <LoadingCover :show="$store.state.Widget.displayLoadingCover"></LoadingCover>
+    <LoadingCover></LoadingCover>
   </div>
 </template>
 
