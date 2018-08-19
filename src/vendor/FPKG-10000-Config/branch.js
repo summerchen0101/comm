734108a821fbf1c7e196711e8530c301978e0f8e
Ruby Chen <mp4bj6ruby@gmail.com>
2018-08-19 09:30:54 +0800

    刪除多餘的config檔案

--- src/vendor/FPKG-10000-Config/branch.js
+++
@@ -1,3 +0,0 @@
-export const branchSource = "default.tw"
-export const branchDomain = "platformapi.thoth-dev.com"
-export const fakeDomain = "localhost:8080/api"
--- src/vendor/FPKG-10000-Config/store/getters.js
+++
@@ -1,8 +0,0 @@
-
-
-import { } from '@/vendor/FPKG-40000-VuexStore/constants'
-
-const getters = {
-};
-
-export default getters
