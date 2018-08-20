3c39a31bbbd9c178b7b482fa29e212caf66eebbb
Ruby Chen <mp4bj6ruby@gmail.com>
2018-08-20 10:27:25 +0800

    刪除Config模組的store引入

--- src/store.js
+++ src/store.js
@@ -4,7 +4,6 @@ import Global from './vendor/FPKG-40000-VuexStore/store'
 import Dashboard from './vendor/FPKG-70000-Dashboard/store'
 import Account from './vendor/FPKG-90000-Account/store'
 import Widget from './vendor/FPKG-110000-Widget/store'
-import Config from './vendor/FPKG-10000-Config/store'
 
 import Member from './vendor/FPKG-130000-Member/store'
 import Announce from './vendor/FPKG-140000-Announce/store'
