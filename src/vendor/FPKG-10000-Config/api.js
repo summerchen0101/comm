c1e29ff2c42c730a0f270585efd5e8a663569ea3
Ruby Chen <mp4bj6ruby@gmail.com>
2018-08-21 21:44:23 +0800

    修改apiPath的判斷方式(by .env)

--- src/vendor/FPKG-10000-Config/api.js
+++ src/vendor/FPKG-10000-Config/api.js
@@ -2,25 +2,14 @@ import axios from 'axios'
 import Store from '@/store'
 import { SWITCH_LOADING_COVER } from '@/vendor/FPKG-40000-VuexStore/constants'
 
-export const branchDomain = psl.parse(location.hostname).domain || location.hostname
+// export const branchDomain = psl.parse(location.hostname).domain || location.hostname
 
-var apiPathByEnv = {
-  local: '/api',
-  dev: 'http://platformapi.thoth-dev.com',
-  beta: 'http://platform-api.gebtest365.net'
-}
-var apiPathMap = [
-  {path: '/api', domain: 'localhost'},
-  {path: 'http://platformapi.thoth-dev.com', domain: 'thoth-dev.com'},
-  {path: 'http://platform-api.gebtest365.net', domain: 'gebtest365.net'},
-]
-var apiIndex = apiPathMap.findIndex(t => branchDomain.search(t.domain) > -1)
+console.log(process.env.VUE_APP_API_ENV)
 export async function apiHub(method, url, data, params) {
   Store.commit(SWITCH_LOADING_COVER, true)
   try {
     var _response = await axios({
-      baseURL: apiPathMap[apiIndex].path,
-      // baseURL: apiPathByEnv[process.env.VUE_APP_API_ENV],
+      baseURL: process.env.VUE_APP_API_PATH,
       method,
       url,
       data,
