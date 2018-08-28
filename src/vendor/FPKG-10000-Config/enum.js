1e6dacac825bd4146bc9b21b0a4dd5c73d464e98
Ruby Chen <mp4bj6ruby@gmail.com>
2018-08-28 16:33:07 +0800

    會員管理: 會員起始級別選項移除「1」

--- src/vendor/FPKG-10000-Config/enum.js
+++ src/vendor/FPKG-10000-Config/enum.js
@@ -30,7 +30,6 @@ export const memberLevelActive = [
   {label: '停用', value: 2},
 ]
 export const memberLevel = [
-  {label: '1', value: 1},
   {label: '2', value: 2},
   {label: '3', value: 3},
   {label: '4', value: 4},
