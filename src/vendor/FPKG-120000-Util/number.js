60f1f1b0065a8e48aeaf4c76594bf12657900f71
Mckey <mckey@ifalo.com.tw>
2018-09-27 01:00:45 +0800

    Fix issue

--- src/vendor/FPKG-120000-Util/number.js
+++ src/vendor/FPKG-120000-Util/number.js
@@ -4,4 +4,8 @@ import _ from 'lodash'
 
 export const toCurrency = (num) => numeral(num).format('0,0');
 export const toCurrencyDecimal = (num, decimalCount = 2) => numeral(num).format('0,0.' + _.repeat('0', decimalCount));
-export const toCurrencyFloor = (num, floorCount = 2) => Math.floor(num * Math.pow(10, floorCount)) / Math.pow(10, floorCount);
+export function toCurrencyFloor(num, floorCount = 2) {
+  const floorPow = 10 ** floorCount
+  let value = Math.floor(num * floorPow) / floorPow
+  return numeral(value).format('0,0.' + _.repeat('0', floorCount))
+}
