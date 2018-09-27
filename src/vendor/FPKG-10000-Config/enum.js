30434071383ad86a77bc77d20839ce49a2f6f98a
Mckey <mckey@ifalo.com.tw>
2018-09-27 10:57:48 +0800

    add delete text

--- src/vendor/FPKG-10000-Config/enum.js
+++ src/vendor/FPKG-10000-Config/enum.js
@@ -55,7 +55,7 @@ export const memberLevel = [
 ]
 export const gameType = [
   {
-    label: '法老王3D電子遊藝',
+    label: '法老王3D電子遊藝[待移除]',
     gameCode: 'casino',
     value: 1,
     children: [
@@ -79,8 +79,8 @@ export const gameType = [
     ]
   },
   {
-    label: '法老王彩球',
-    gameCode: 'lotteryx',
+    label: '法老王彩球[待移除]',
+    gameCode: 'lottery',
     value: 2,
     children: [
       {label: "六合彩", value: 1},
@@ -91,13 +91,13 @@ export const gameType = [
     ]
   },
   {
-    label: '法老王彩票x',
+    label: '法老王彩票[待移除]',
     gameCode: 'ifalo_lottery',
     value: 3,
     children: []
   },
   {
-    label: '法老王黃金期權x',
+    label: '法老王黃金期權[待移除]',
     gameCode: 'ifalo_golden',
     value: 4,
     children: []
