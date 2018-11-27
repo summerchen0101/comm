7ac465943cd8f6390e476ba63397231d57d9f173
Will <will@ifalo.com.tw>
2018-11-27 13:17:25 +0800

    新增資訊顯示

--- src/vendor/FPKG-70000-Dashboard/component/HeaderMenuInfo.vue
+++ src/vendor/FPKG-70000-Dashboard/component/HeaderMenuInfo.vue
@@ -1,6 +1,17 @@
 <template>
-  <div id="HeaderMenuInfo">
-    <!-- <el-dropdown trigger="click" style="cursor: pointer" @command="onClick">
+<div id="HeaderMenuInfo">
+  <div style="display:inline-block; margin:0px 8px;">
+    <span>今日：</span>
+    <span style="display:inline-block; margin:0px 8px;">
+    <el-button type="warning">註冊會員 {{today.register}}</el-button>
+    <el-button type="danger">存款點數 {{today.deposit}}</el-button>
+    <el-button type="success">提款點數 {{today.withdrawal}}</el-button>
+    </span>
+    <span style="display:inline-block; margin:0px 4px;">{{count}}</span>
+    <span style="display:inline-block; margin:0px 4px;"><font-awesome-icon icon="sync-alt" :spin="isLoading" @click="getTodayList" /></span>
+  </div>
+
+  <!-- <el-dropdown trigger="click" style="cursor: pointer" @command="onClick">
       <i class="el-icon-menu" style="margin-right: 15px"></i>
       <el-dropdown-menu slot="dropdown">
         <el-dropdown-item v-for="m,i in menu" :key="m.name" :command="m.name" :class="{'sys-menu-active': matchRoute(m.name)}">
@@ -11,33 +22,52 @@
         </el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown> -->
-    <span class="username mr-3 pointer" @click="onUserNameClicked">{{userInfo.name}}</span>
-    <router-link :to="{name: 'Logout'}" style="color: #fff"><font-awesome-icon icon="sign-out-alt" /></router-link>
-    <!-- <el-dropdown trigger="click" style="cursor: pointer" @command="onClick">
+  <span class="username mr-3 pointer" @click="onUserNameClicked">{{userInfo.name}}</span>
+  <router-link :to="{name: 'Logout'}" style="color: #fff">
+    <font-awesome-icon icon="sign-out-alt" />
+  </router-link>
+  <!-- <el-dropdown trigger="click" style="cursor: pointer" @command="onClick">
       <span class="username">{{userInfo.name}}</span>
       <el-dropdown-menu slot="dropdown">
         <el-dropdown-item command="PersonalProfile">個人資訊</el-dropdown-item>
         <el-dropdown-item command="Logout">登出</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown> -->
-    
-  </div>
+
+</div>
 </template>
 
 <script>
 import HeaderSysMenu from './HeaderSysMenu.vue'
-import { USER_INFO, SWITCH_PASSWD_DIALOG } from '@/vendor/FPKG-40000-VuexStore/constants'
-import { mapState, mapGetters, mapActions, mapMutations } from 'vuex'
+import {
+  USER_INFO,
+  SWITCH_PASSWD_DIALOG,
+  GET_TODAY_LIST
+} from '@/vendor/FPKG-40000-VuexStore/constants'
+import {
+  mapState,
+  mapGetters,
+  mapActions,
+  mapMutations
+} from 'vuex'
 
 export default {
   components: {
     HeaderSysMenu: HeaderSysMenu,
   },
   props: [],
+  data() {
+    return {
+      count: 60,
+      interval: null
+    }
+  },
   computed: {
     ...mapState({
       // showSysMenu: state => state.Dashboard.isShowWebSysMenu,
-      menu: state => state.Dashboard.menu
+      menu: state => state.Dashboard.menu,
+      today: state => state.Dashboard.today,
+      isLoading: state => state.Dashboard.isLoadingToday,
     }),
     ...mapGetters({
       userInfo: USER_INFO
@@ -45,24 +75,42 @@ export default {
   },
   methods: {
     onClick(target) {
-      this.$router.push({name: target})
+      this.$router.push({
+        name: target
+      })
     },
     matchRoute(name) {
       return this.$route.matched.findIndex(r => r.name === name) > -1
     },
     onUserNameClicked() {
       this.$store.commit(SWITCH_PASSWD_DIALOG, true)
+    },
+    async getTodayList() {
+      await this.$store.dispatch(GET_TODAY_LIST)
+      this.count = 60
+    },
+    async tick() {
+      this.count--
+      if (this.count === 0) {
+        await this.getTodayList()
+      }
+      this.interval = setTimeout(this.tick, 1000)
     }
   },
   mounted() {
+    this.getTodayList()
+    this.tick()
+  },
+  beforeDestroy() {
+    clearTimeout(this.interval)
   }
-  
-  
 }
 </script>
 
 <style lang="stylus">
-.el-dropdown-menu__item.sys-menu-active, .el-dropdown-menu__item:not(.is-disabled):hover, .el-dropdown-menu__item:focus {
+.el-dropdown-menu__item.sys-menu-active,
+.el-dropdown-menu__item:not(.is-disabled):hover,
+.el-dropdown-menu__item:focus {
   color: $TME-100-second-color;
 }
 
--- src/vendor/FPKG-70000-Dashboard/store/actions.js
+++ src/vendor/FPKG-70000-Dashboard/store/actions.js
@@ -1,8 +1,19 @@
-import { GET_USER_INFO } from '@/vendor/FPKG-40000-VuexStore/constants'
+import { GET_USER_INFO,
+  GET_TODAY_LIST,
+  GOT_TODAY_LIST,
+  SWITCH_TODAY_LOADING_STATUS,
+ } from '@/vendor/FPKG-40000-VuexStore/constants'
 import { apiHub } from '@/vendor/FPKG-10000-Config/api'
 
 const actions = {
-  
+  async [GET_TODAY_LIST]({commit}) {
+    commit(SWITCH_TODAY_LOADING_STATUS, true)
+    let res = await apiHub("get", "api/v1/home/today")
+    if(res.code === 200001) {
+      commit(GOT_TODAY_LIST, res.result)
+      commit(SWITCH_TODAY_LOADING_STATUS, false)
+    }
+  }
 }
 
 export default actions
