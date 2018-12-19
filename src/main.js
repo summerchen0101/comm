c22459c6c9abf510d8d1ff064de579fdd074cca5
Ruby Chen <mp4bj6ruby@gmail.com>
2018-12-19 12:09:10 +0800

    首頁待審數加上音效提示

--- src/main.js
+++ src/main.js
@@ -51,10 +51,9 @@ Vue.config.productionTip = false
 
 import commonTool from '@/vendor/FPKG-120000-Util/mixins/commonTool'
 
-new Vue({
+let myVue = new Vue({
   mixins: [commonTool],
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
-
