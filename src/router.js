36a65571199ae3712646cba470053074cd5db033
Will <will@ifalo.com.tw>
2019-01-14 16:06:38 +0800

    修改ip檢測作法

--- src/router.js
+++ src/router.js
@@ -6,8 +6,8 @@ import {
   SWITCH_WEB_SYS_MENU,
   BRANCH_INFO,
   GET_BRANCH_INFO,
-  SET_USER_AUTH,
- } from '@/vendor/FPKG-40000-VuexStore/constants'
+  SET_USER_AUTH
+} from '@/vendor/FPKG-40000-VuexStore/constants'
 import Dashboard from '@/vendor/FPKG-70000-Dashboard/router'
 import { Login, Logout } from '@/vendor/FPKG-90000-Account/router'
 import { Maintenance } from '@/vendor/FPKG-110000-Widget/router'
@@ -18,7 +18,7 @@ import {
   MemberSubtractedPoint,
   MemberDirectMembers,
   MemberUpperMembers,
-  BankAcc,
+  BankAcc
 } from '@/vendor/FPKG-130000-Member/router'
 
 import { Deposit, Withdraw, Dispense } from '@/vendor/FPKG-150000-Money/router'
@@ -32,7 +32,7 @@ const RouterSetting = new Router({
     {
       path: '/',
       name: 'Index',
-      redirect: {name: 'Home'},
+      redirect: { name: 'Home' }
     },
     Dashboard,
     Login,
@@ -56,51 +56,44 @@ let tw = null
 
 RouterSetting.beforeEach(async (to, from, next) => {
   if (tw === null) {
-    if (process.env.VUE_APP_API_ENV !== 'production') {
-      tw = false
-    }
-    else {
+    //if (process.env.VUE_APP_API_ENV !== 'production') {
+    //  tw = false
+    //} else {
       tw = true
-      const res = await axios.get('https://ipapi.co/json/')
-      if (res && res.data && res.data.country !== 'TW') {
+      const res = await geoplugin_countryCode()
+      if (res !== 'TW') {
         tw = false
       }
-    }
+    //}
   }
 
   const Store = RouterSetting.app.$store
 
   Store.commit(SWITCH_WEB_SYS_MENU, false)
   // 若無Auth
-  if(!Store.state.Account.auth) {
+  if (!Store.state.Account.auth) {
     Store.commit(SET_USER_AUTH)
   }
 
   // 進入內頁時若未登入 Y-> 獲取使用者資訊
-  if (to.name === "NoService") {
+  if (to.name === 'NoService') {
     next()
-  }
-  else if (tw) {
-    next({name: "NoService"})
-  }
-  else if(Store.getters.IS_LOGIN !== true && PublicPages.indexOf(to.name) === -1) {
+  } else if (tw) {
+    next({ name: 'NoService' })
+  } else if (
+    Store.getters.IS_LOGIN !== true &&
+    PublicPages.indexOf(to.name) === -1
+  ) {
     await Store.dispatch(GET_USER_INFO)
     // 能取得登入資訊 Y-> 開啟畫面 N-> 無則倒回登入頁
-    if(Store.getters.IS_LOGIN) {
+    if (Store.getters.IS_LOGIN) {
       next()
-    }else {
-      next({name: "Login"})
+    } else {
+      next({ name: 'Login' })
     }
-
-  }
-  else {
+  } else {
     next()
   }
-
-
 })
 
 export default RouterSetting
-
-
-
