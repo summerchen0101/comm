ded0a31a47bb4b2620f6115bde91152e0c815ef1
Ruby Chen <mp4bj6ruby@gmail.com>
2019-02-20 22:11:00 +0800

    Util: 微調toCurrency(加上'-'例外)

--- src/vendor/FPKG-120000-Util/number.js
+++ src/vendor/FPKG-120000-Util/number.js
@@ -2,7 +2,7 @@ import numeral from 'numeral'
 import Vue from 'vue'
 import _ from 'lodash'
 
-export const toCurrency = (num) => numeral(num).format('0,0');
+export const toCurrency = (num) => num === '-' ? '-' : numeral(num).format('0,0');
 export const toCurrencyDecimal = (num, decimalCount = 2) => numeral(num).format('0,0.' + _.repeat('0', decimalCount));
 export function toCurrencyFloor(num, floorCount = 2) {
   const floorPow = Math.pow(10, floorCount)
